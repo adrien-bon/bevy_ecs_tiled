9443b470e9cd8a45bb1e8d52ebeba4f3381d7d95
Steve Pryde <steve@stevepryde.com>
2024-07-13 21:31:50 +0930

    Update README

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # bevy_ecs_tiled
 
-Plugin for working with 2D tilemaps created with the Tiled map editor
+Plugin for working with 2D tilemaps created with the Tiled map editor.
 
 ## Status
 
@@ -9,15 +9,66 @@ Plugin for working with 2D tilemaps created with the Tiled map editor
 This crate is currently in a very early state while I build the basics.
 I'm still new to Bevy so I'm learning as I go.
 
+The code was originally copied from the Tiled example in `bevy_ecs_tilemap`,
+plus the fix from [this PR](https://github.com/StarArawn/bevy_ecs_tilemap/pull/429).
+
 Contributions are welcome.
+
+## Features
+
+- Finite and infinite maps
+- Embedded and separate tileset
+
+## Getting started
+
+Add this crate to your Cargo.toml. For Bevy 0.14 this requires a git 
+dependency until a compatible release is published for bevy_ecs_tilemap.
+
+```toml
+[dependencies]
+bevy = {version = "0.14.0"}
+bevy_ecs_tiled = {git = "https://github.com/stevepryde/bevy_ecs_tiled"}
+bevy_ecs_tilemap = {git = "https://github.com/StarArawn/bevy_ecs_tilemap"}
+```
+
+Then add the plugin to your app:
+
+```rust
+use bevy::prelude::*;
+use bevy_ecs_tiled::prelude::*;
+use bevy_ecs_tilemap::prelude::*;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugins(TilemapPlugin)
+        .add_plugins(TiledMapPlugin)
+        .add_systems(Startup, startup)
+        .run();
+}
+
+fn startup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera2dBundle::default());
+
+    // Ensure any tile / tileset paths are relative to assets/
+    let map_handle: Handle<TiledMap> = asset_server.load("map.tmx");
+    commands.spawn(TiledMapBundle {
+        tiled_map: map_handle,
+        ..Default::default()
+    });
+}
+```
 
 ## Bevy Compatibility
 
 |bevy|bevy_ecs_tilemap|bevy_ecs_tiled|
 |---|---|---|
 |0.12|0.12|0.1|
-|0.13|main@e4f3cc6|0.2|
-|0.14|main|0.3|
+|0.13|main@e4f3cc6|0.2 *|
+|0.14|main|main (0.3 *)|
+
+\* These versions are not yet published to crates.io as they are waiting on 
+  upstream versions to be published. Use git dependencies instead.
 
 ## LICENSE
 
---
+++ examples/helper/camera.rs
@@ -0,0 +1,75 @@
+// The code in this file was originally copied from
+// [bevy_ecs_tilemap](https://github.com/StarArawn/bevy_ecs_tilemap).
+// The original code is licensed under the following license,
+// with modifications under the license in the root of this repository.
+//
+// --
+// MIT License
+
+// Copyright (c) 2021 John
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+use bevy::{input::ButtonInput, math::Vec3, prelude::*, render::camera::Camera};
+
+// A simple camera system for moving and zooming the camera.
+#[allow(dead_code)]
+pub fn movement(
+    time: Res<Time>,
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    mut query: Query<(&mut Transform, &mut OrthographicProjection), With<Camera>>,
+) {
+    for (mut transform, mut ortho) in query.iter_mut() {
+        let mut direction = Vec3::ZERO;
+
+        if keyboard_input.pressed(KeyCode::KeyA) {
+            direction -= Vec3::new(1.0, 0.0, 0.0);
+        }
+
+        if keyboard_input.pressed(KeyCode::KeyD) {
+            direction += Vec3::new(1.0, 0.0, 0.0);
+        }
+
+        if keyboard_input.pressed(KeyCode::KeyW) {
+            direction += Vec3::new(0.0, 1.0, 0.0);
+        }
+
+        if keyboard_input.pressed(KeyCode::KeyS) {
+            direction -= Vec3::new(0.0, 1.0, 0.0);
+        }
+
+        if keyboard_input.pressed(KeyCode::KeyZ) {
+            ortho.scale += 0.1;
+        }
+
+        if keyboard_input.pressed(KeyCode::KeyX) {
+            ortho.scale -= 0.1;
+        }
+
+        if ortho.scale < 0.5 {
+            ortho.scale = 0.5;
+        }
+
+        let z = transform.translation.z;
+        transform.translation += time.delta_seconds() * direction * 500.;
+        // Important! We need to restore the Z values when moving the camera around.
+        // Bevy has a specific camera setup and this can mess with how our layers are shown.
+        transform.translation.z = z;
+    }
+}
