f5fded5f10dd52f586a4594c6baf6556cd7b50b5
Steve Pryde <steve@stevepryde.com>
2024-07-13 21:11:07 +0930

    Add more examples

---
+++ assets/Tileset1.tsx
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Tileset1" tilewidth="32" tileheight="32" tilecount="7" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="tiles/tile0.png" width="32" height="32"/>
+ </tile>
+ <tile id="1">
+  <image source="tiles/tile1.png" width="32" height="32"/>
+ </tile>
+ <tile id="2">
+  <image source="tiles/tile2.png" width="32" height="32"/>
+ </tile>
+ <tile id="3">
+  <image source="tiles/tile3.png" width="32" height="32"/>
+ </tile>
+ <tile id="4">
+  <image source="tiles/tile4.png" width="32" height="32"/>
+ </tile>
+ <tile id="5">
+  <image source="tiles/tile5.png" width="32" height="32"/>
+ </tile>
+ <tile id="6">
+  <image source="tiles/tile6.png" width="32" height="32"/>
+ </tile>
+</tileset>
---
+++ examples/infinite.rs
@@ -0,0 +1,25 @@
+use bevy::prelude::*;
+use bevy_ecs_tiled::prelude::*;
+use bevy_ecs_tilemap::prelude::*;
+
+mod helper;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugins(TilemapPlugin)
+        .add_plugins(TiledMapPlugin)
+        .add_systems(Startup, startup)
+        .add_systems(Update, helper::movement)
+        .run();
+}
+
+fn startup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera2dBundle::default());
+
+    let map_handle: Handle<TiledMap> = asset_server.load("infinite.tmx");
+    commands.spawn(TiledMapBundle {
+        tiled_map: map_handle,
+        ..Default::default()
+    });
+}
