f007ae32d98667969dfa7589c1b1752fe2130e32
Adrien Bon <63343541+adrien-bon@users.noreply.github.com>
2024-07-24 10:27:45 +0000

    feat: add a new example with an hexagonal map with several layers and animated tiles

---
+++ assets/README.md
@@ -0,0 +1,3 @@
+# Assets list
+
+- drjamgo_hex_16x16 : downloaded from [OpenGameArt.org](https://opengameart.org/content/basic-hex-tile-set-16x16)
---
+++ assets/drjamgo_hex_16x16.tsx
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="drjamgo_hex_16x16" tilewidth="16" tileheight="16" tilecount="20" columns="4">
+ <image source="tiles/drjamgo_hex_16x16.png" width="64" height="80"/>
+ <tile id="19">
+  <animation>
+   <frame tileid="12" duration="200"/>
+   <frame tileid="13" duration="200"/>
+   <frame tileid="14" duration="200"/>
+   <frame tileid="15" duration="200"/>
+  </animation>
+ </tile>
+</tileset>
