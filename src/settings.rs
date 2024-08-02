ee3edafbc73827441e8772c558c23cc49ded3875
durango <me@danieldurante.com>
2024-08-02 15:18:39 -0400

    Adds support for transforming TilemapBundle using (0, 0) as the center

--- src/settings.rs
+++ src/settings.rs
@@ -10,4 +10,18 @@ pub struct TiledMapSettings {
     pub collision_layer_names: ObjectNames,
     /// Specify which tileset object names to add collision shapes for.
     pub collision_object_names: ObjectNames,
+    /// Specify which position transformation offset should be applied.
+    ///
+    /// By default, the layer's offset will be used.
+    /// For Bevy's coordinate system use MapPositioning::Centered
+    pub map_positioning: MapPositioning,
+}
+
+#[derive(Default, Clone)]
+pub enum MapPositioning {
+    #[default]
+    /// Transforms TilemapBundle starting from the layer's offset.
+    LayerOffset,
+    /// Mimics Bevy's coordinate system so that (0, 0) is at the center of the map.
+    Centered,
 }
