b833ac35cc79e7d4e51e94de2a6fc92a228bb059
Adrien BON <63343541+adrien-bon@users.noreply.github.com>
2024-11-05 19:46:27 +0000

    ci: check the crate using all features combinations

--- ci_check.sh
+++ ci_check.sh
@@ -4,3 +4,5 @@ cargo clippy --workspace --all-targets --all-features -- --deny warnings || { ec
 cargo fmt --all -- --check || { echo "cargo fmt failed"; exit 1; }
 cargo test --workspace --all-features --all-targets || { echo "cargo test failed"; exit 1; }
 cargo test --workspace --doc --all-features || { echo "cargo test --doc failed"; exit 1; }
+cargo check-all-features || { echo "cargo check-all-features failed"; exit 1; }
+echo "All OK! :)"
