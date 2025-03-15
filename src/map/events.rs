3cda567fa7ff37c4d61784799df353cb6ddd54c9
Adrien BON <63343541+adrien-bon@users.noreply.github.com>
2025-03-15 12:15:31 +0000

    chore: fix clippy

--- src/map/events.rs
+++ src/map/events.rs
@@ -23,7 +23,7 @@ pub struct TiledMapEventWriters<'w> {
     pub tile_event: EventWriter<'w, TiledTileCreated>,
 }
 
-impl<'w> fmt::Debug for TiledMapEventWriters<'w> {
+impl fmt::Debug for TiledMapEventWriters<'_> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         f.debug_struct("TiledMapEventWriters").finish()
     }
