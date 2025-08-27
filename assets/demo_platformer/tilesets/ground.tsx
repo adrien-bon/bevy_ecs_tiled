2159826fc2c742382f987ffce4d3e52c1a61385d
Adrien BON <63343541+adrien-bon@users.noreply.github.com>
2025-08-27 12:18:41 +0000

    wip: demo platformer

---
+++ assets/demo_platformer/tilesets/ground.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="spritesheet_ground" tilewidth="128" tileheight="128" tilecount="128" columns="8">
+ <image source="../kenney_platformer-pack-redux/Spritesheets/spritesheet_ground.png" width="1024" height="2048"/>
+</tileset>
