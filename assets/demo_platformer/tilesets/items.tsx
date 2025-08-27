035f890ae6cd630803adae94e523f36b09848db9
Adrien BON <63343541+adrien-bon@users.noreply.github.com>
2025-08-27 12:18:41 +0000

    wip: demo platformer

---
+++ assets/demo_platformer/tilesets/items.tsx
@@ -0,0 +1,92 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="items" tilewidth="128" tileheight="128" tilecount="24" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="../kenney_platformer-pack-redux/PNG/Items/coinBronze.png" width="128" height="128"/>
+ </tile>
+ <tile id="1">
+  <image source="../kenney_platformer-pack-redux/PNG/Items/coinGold.png" width="128" height="128"/>
+ </tile>
+ <tile id="2">
+  <image source="../kenney_platformer-pack-redux/PNG/Items/coinSilver.png" width="128" height="128"/>
+ </tile>
+ <tile id="3">
+  <image source="../kenney_platformer-pack-redux/PNG/Items/flagBlue_down.png" width="128" height="128"/>
+ </tile>
+ <tile id="4">
+  <image source="../kenney_platformer-pack-redux/PNG/Items/flagBlue1.png" width="128" height="128"/>
+  <animation>
+   <frame tileid="4" duration="100"/>
+   <frame tileid="5" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="5">
+  <image source="../kenney_platformer-pack-redux/PNG/Items/flagBlue2.png" width="128" height="128"/>
+ </tile>
+ <tile id="6">
+  <image source="../kenney_platformer-pack-redux/PNG/Items/flagGreen_down.png" width="128" height="128"/>
+ </tile>
+ <tile id="7">
+  <image source="../kenney_platformer-pack-redux/PNG/Items/flagGreen1.png" width="128" height="128"/>
+  <animation>
+   <frame tileid="7" duration="100"/>
+   <frame tileid="8" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="8">
+  <image source="../kenney_platformer-pack-redux/PNG/Items/flagGreen2.png" width="128" height="128"/>
+ </tile>
+ <tile id="9">
+  <image source="../kenney_platformer-pack-redux/PNG/Items/flagRed_down.png" width="128" height="128"/>
+ </tile>
+ <tile id="10">
+  <image source="../kenney_platformer-pack-redux/PNG/Items/flagRed1.png" width="128" height="128"/>
+  <animation>
+   <frame tileid="10" duration="100"/>
+   <frame tileid="11" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="11">
+  <image source="../kenney_platformer-pack-redux/PNG/Items/flagRed2.png" width="128" height="128"/>
+ </tile>
+ <tile id="12">
+  <image source="../kenney_platformer-pack-redux/PNG/Items/flagYellow_down.png" width="128" height="128"/>
+ </tile>
+ <tile id="13">
+  <image source="../kenney_platformer-pack-redux/PNG/Items/flagYellow1.png" width="128" height="128"/>
+  <animation>
+   <frame tileid="13" duration="100"/>
+   <frame tileid="14" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="14">
+  <image source="../kenney_platformer-pack-redux/PNG/Items/flagYellow2.png" width="128" height="128"/>
+ </tile>
+ <tile id="15">
+  <image source="../kenney_platformer-pack-redux/PNG/Items/gemBlue.png" width="128" height="128"/>
+ </tile>
+ <tile id="16">
+  <image source="../kenney_platformer-pack-redux/PNG/Items/gemGreen.png" width="128" height="128"/>
+ </tile>
+ <tile id="17">
+  <image source="../kenney_platformer-pack-redux/PNG/Items/gemRed.png" width="128" height="128"/>
+ </tile>
+ <tile id="18">
+  <image source="../kenney_platformer-pack-redux/PNG/Items/gemYellow.png" width="128" height="128"/>
+ </tile>
+ <tile id="19">
+  <image source="../kenney_platformer-pack-redux/PNG/Items/keyBlue.png" width="128" height="128"/>
+ </tile>
+ <tile id="20">
+  <image source="../kenney_platformer-pack-redux/PNG/Items/keyGreen.png" width="128" height="128"/>
+ </tile>
+ <tile id="21">
+  <image source="../kenney_platformer-pack-redux/PNG/Items/keyRed.png" width="128" height="128"/>
+ </tile>
+ <tile id="22">
+  <image source="../kenney_platformer-pack-redux/PNG/Items/keyYellow.png" width="128" height="128"/>
+ </tile>
+ <tile id="23">
+  <image source="../kenney_platformer-pack-redux/PNG/Items/star.png" width="128" height="128"/>
+ </tile>
+</tileset>
---
+++ examples/demo_platformer/helper/mod.rs
@@ -0,0 +1,66 @@
+use bevy::{input::common_conditions::input_toggle_active, prelude::*};
+use bevy_inspector_egui::{bevy_egui::EguiPlugin, quick::WorldInspectorPlugin};
+use iyes_perf_ui::prelude::*;
+
+pub mod anchor;
+#[allow(dead_code)]
+pub mod assets;
+
+mod camera;
+mod map;
+
+#[derive(Default)]
+pub struct HelperPlugin;
+
+impl Plugin for HelperPlugin {
+    fn build(&self, app: &mut bevy::prelude::App) {
+        app.add_plugins((
+            EguiPlugin::default(),
+            WorldInspectorPlugin::default().run_if(input_toggle_active(false, KeyCode::Escape)),
+            // we want Bevy to measure these values for us:
+            bevy::diagnostic::FrameTimeDiagnosticsPlugin::default(),
+            bevy::diagnostic::EntityCountDiagnosticsPlugin,
+            bevy::diagnostic::SystemInformationDiagnosticsPlugin,
+            bevy::render::diagnostic::RenderDiagnosticsPlugin,
+            PerfUiPlugin,
+        ));
+        app.add_systems(Startup, setup_help_text);
+        app.add_systems(Update, camera::movement);
+        app.add_systems(Update, map::rotate);
+        app.add_systems(
+            Update,
+            toggle_perf_ui.before(iyes_perf_ui::PerfUiSet::Setup),
+        );
+    }
+}
+
+fn setup_help_text(mut commands: Commands) {
+    commands
+        .spawn(Node {
+            display: Display::Flex,
+            align_self: AlignSelf::FlexEnd,
+            flex_direction: FlexDirection::Column,
+            ..default()
+        })
+        .with_children(|builder| {
+            builder.spawn(Text(String::from("Toggle inspector: [Esc]")));
+            builder.spawn(Text(String::from("Toggle iyes_perf_ui: [Backspace]")));
+            builder.spawn(Text(String::from("Pan camera: [W/A/S/D]")));
+            builder.spawn(Text(String::from("Zoom camera: [Z/X]")));
+            builder.spawn(Text(String::from("Rotate map / world: [Q/E]")));
+        });
+}
+
+fn toggle_perf_ui(
+    mut commands: Commands,
+    q_perf_ui: Query<Entity, With<PerfUiRoot>>,
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+) {
+    if keyboard_input.just_pressed(KeyCode::Backspace) {
+        if let Ok(e) = q_perf_ui.single() {
+            commands.entity(e).despawn();
+        } else {
+            commands.spawn(PerfUiAllEntries::default());
+        }
+    }
+}
