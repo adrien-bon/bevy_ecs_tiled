8925ea9b3ffa85774261a05f12a69f93658bfac9
Adrien BON <63343541+adrien-bon@users.noreply.github.com>
2025-09-10 14:36:57 +0000

    debug: add an helper text and remap inspector / perf_ui toggle keys

--- examples/demo_platformer/debug.rs
+++ examples/demo_platformer/debug.rs
@@ -3,8 +3,8 @@ use bevy::{input::common_conditions::input_toggle_active, prelude::*};
 use bevy_inspector_egui::{bevy_egui::EguiPlugin, quick::WorldInspectorPlugin};
 use iyes_perf_ui::prelude::*;
 
-const TOGGLE_INSPECTOR_KEY: KeyCode = KeyCode::F2;
-const TOGGLE_IYES_PERF_KEY: KeyCode = KeyCode::F3;
+const TOGGLE_INSPECTOR_KEY: KeyCode = KeyCode::F1;
+const TOGGLE_IYES_PERF_KEY: KeyCode = KeyCode::F2;
 
 pub(super) fn plugin(app: &mut App) {
     app.add_plugins((
@@ -19,11 +19,28 @@ pub(super) fn plugin(app: &mut App) {
         avian2d::prelude::PhysicsDebugPlugin::default(),
     ));
 
+    app.add_systems(Startup, setup_help_text);
+
     // Toggle the debug overlay for UI.
     app.add_systems(
         Update,
         (toggle_perf_ui.before(iyes_perf_ui::PerfUiSet::Setup),),
     );
+}
+
+fn setup_help_text(mut commands: Commands) {
+    commands
+        .spawn(Node {
+            display: Display::Flex,
+            align_self: AlignSelf::FlexEnd,
+            flex_direction: FlexDirection::Column,
+            ..default()
+        })
+        .with_children(|builder| {
+            builder.spawn(Text(String::from("Debug mode enabled")));
+            builder.spawn(Text(String::from("Toggle inspector: [F1]")));
+            builder.spawn(Text(String::from("Toggle iyes_perf_ui: [F2]")));
+        });
 }
 
 fn toggle_perf_ui(
