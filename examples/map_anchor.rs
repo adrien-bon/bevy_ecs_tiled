3c7aebca6d4818d17f8c6e51a015b5fdf9908e7e
Adrien BON <63343541+adrien-bon@users.noreply.github.com>
2025-09-28 09:31:51 +0000

    s/JustifyText/Justify

--- examples/map_anchor.rs
+++ examples/map_anchor.rs
@@ -49,7 +49,7 @@ fn startup(mut commands: Commands, asset_server: Res<AssetServer>) {
                 font_size,
                 ..default()
             },
-            TextLayout::new_with_justify(JustifyText::Left),
+            TextLayout::new_with_justify(Justify::Left),
             AnchorLabel,
         ))
         .with_children(|parent| {
