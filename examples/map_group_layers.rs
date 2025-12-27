0951961b18115ab1064ee88e79e11f42ba92dbb3
syynth <ben.cochrane2112@gmail.com>
2025-12-27 11:41:10 -0500

    Implement group layers
    - Add recursive 'spawn_layers' function
    - Add process_layers in loader.rs
    - key image layers based off layer.id() in both

---
+++ examples/map_group_layers.rs
@@ -0,0 +1,35 @@
+//! This example shows the basic usage of the plugin.
+
+use bevy::prelude::*;
+use bevy_ecs_tiled::prelude::*;
+
+mod helper;
+
+fn main() {
+    App::new()
+        // Bevy default plugins: prevent blur effect by changing default sampling
+        .add_plugins(DefaultPlugins.build().set(ImagePlugin::default_nearest()))
+        // Add bevy_ecs_tiled plugin: bevy_ecs_tilemap::TilemapPlugin will
+        // be automatically added as well if it's not already done
+        .add_plugins(TiledPlugin::default())
+        // Examples helper plugins, such as the logic to pan and zoom the camera
+        // This should not be used directly in your game (but you can always have a look)
+        .add_plugins(helper::HelperPlugin)
+        // Add our systems and run the app!
+        .add_systems(Startup, startup)
+        .run();
+}
+
+fn startup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    // Spawn a 2D camera (required by Bevy)
+    commands.spawn(Camera2d);
+
+    // Load a map then spawn it
+    commands.spawn((
+        // Only the [`TiledMap`] component is actually required to spawn a map.
+        TiledMap(asset_server.load("maps/orthogonal/group_layers.tmx")),
+        // But you can add extra components to change the defaults settings and how
+        // your map is actually displayed
+        TilemapAnchor::Center,
+    ));
+}
