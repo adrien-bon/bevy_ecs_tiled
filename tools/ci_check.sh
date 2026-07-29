12654aa8d002fcd7460781e0d93560cf9e15b04f
Adrien Bon <63343541+adrien-bon@users.noreply.github.com>
2026-07-29 09:54:33 +0200

    feat(#222): use just for project commands (#229)

--- tools/ci_check.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cargo clippy --workspace --all-targets --all-features -- --deny warnings || { echo "cargo clippy failed"; exit 1; }
-cargo fmt --all -- --check || { echo "cargo fmt failed"; exit 1; }
-cargo test --workspace --all-features --all-targets || { echo "cargo test failed"; exit 1; }
-cargo test --workspace --doc --all-features || { echo "cargo test --doc failed"; exit 1; }
-cargo check-all-features || { echo "cargo check-all-features failed"; exit 1; }
-echo "All OK! :)"
